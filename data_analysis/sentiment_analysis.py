bcc403d1b53df588b783d82d44a694c905969aef
Luke Terrell <luke.terrell43@gmail.com>
2023-02-08 20:22:44 -0700

    added pseudo framework

---
+++ data_analysis/sentiment_analysis.py
@@ -0,0 +1,7 @@
+# This file will analyze and make predictions on stock performance based on features collected from financial websites.
+# Dependencies
+
+# import the data from database
+# separate into two categories 'positive' and 'negative'
+# pull coresponding prices related to tickers at certain datetimes after sentimental assignment date
+# Analyze the possible trends/correlations, possible machine-learning opportunity
